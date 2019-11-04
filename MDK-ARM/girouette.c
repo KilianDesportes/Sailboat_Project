60d10f1de1f8eeba8477fc7c1fc419a7edcb1343
Kilian Desportes <desporte@insat.insa-toulouse.fr>
2019-11-04 15:30:53 +0100

    girouette

--- MDK-ARM/girouette.c
+++ MDK-ARM/girouette.c
@@ -8,31 +8,52 @@ void Girouette_Conf(TIM_TypeDef * Timer)
 {
 	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
 	
-	LL_GPIO_InitTypeDef GPIO_Init = {	LL_GPIO_PIN_8,
+	LL_GPIO_InitTypeDef GPIO_Init_5 = {	LL_GPIO_PIN_5,
+		LL_GPIO_MODE_ALTERNATE,
+		LL_GPIO_MODE_OUTPUT_2MHz,
+		LL_GPIO_OUTPUT_PUSHPULL,
+		LL_GPIO_PULL_UP };
+	
+	LL_GPIO_InitTypeDef GPIO_Init_6 = {	LL_GPIO_PIN_6,
+		LL_GPIO_MODE_ALTERNATE,
+		LL_GPIO_MODE_OUTPUT_2MHz,
+		LL_GPIO_OUTPUT_PUSHPULL,
+		LL_GPIO_PULL_UP };
+	
+	LL_GPIO_InitTypeDef GPIO_Init_7 = {	LL_GPIO_PIN_7,
 		LL_GPIO_MODE_ALTERNATE,
 		LL_GPIO_MODE_OUTPUT_2MHz,
 		LL_GPIO_OUTPUT_PUSHPULL,
 		LL_GPIO_PULL_UP };
 		
-	LL_GPIO_Init( GPIOA, &GPIO_Init );
+	LL_GPIO_Init( GPIOA, &GPIO_Init_5 );
+	LL_GPIO_Init( GPIOA, &GPIO_Init_6 );
+	LL_GPIO_Init( GPIOA, &GPIO_Init_7 );
 	
-	LL_TIM_InitTypeDef My_LL_Tim_Init_Struct;
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM3);
+		
+	LL_TIM_ENCODER_InitTypeDef My_LL_Tim_Encoder_Init_Struct;
+		
+	My_LL_Tim_Encoder_Init_Struct.EncoderMode = LL_TIM_ENCODERMODE_X4_TI12;
+	My_LL_Tim_Encoder_Init_Struct.IC1Polarity = LL_TIM_CHANNEL_CH1;
+	My_LL_Tim_Encoder_Init_Struct.IC1ActiveInput = LL_TIM_CHANNEL_CH1;
+	My_LL_Tim_Encoder_Init_Struct.IC1Prescaler = LL_TIM_CHANNEL_CH1;
+	My_LL_Tim_Encoder_Init_Struct.IC1Filter = LL_TIM_CHANNEL_CH1;
+	My_LL_Tim_Encoder_Init_Struct.IC2Polarity = LL_TIM_CHANNEL_CH2;
+	My_LL_Tim_Encoder_Init_Struct.IC2ActiveInput = LL_TIM_CHANNEL_CH2;
+	My_LL_Tim_Encoder_Init_Struct.IC2Prescaler =
+	My_LL_Tim_Encoder_Init_Struct.IC2Filter =
+
 	
-	LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_TIM1);
-	
-	int ARR = 9999;
-	int PSC = 143;
-	
-	My_LL_Tim_Init_Struct.Autoreload = ARR;
-	My_LL_Tim_Init_Struct.Prescaler = PSC;
-	
-	LL_TIM_Init(Timer,&My_LL_Tim_Init_Struct);
+	LL_TIM_Init(Timer,&My_LL_Tim_Encoder_Init_Struct);
 	
 	LL_TIM_CC_EnableChannel(Timer,LL_TIM_CHANNEL_CH1);
+	LL_TIM_CC_EnableChannel(Timer,LL_TIM_CHANNEL_CH2);
 	
-	LL_TIM_OC_SetMode(Timer,LL_TIM_CHANNEL_CH1,LL_TIM_OCMODE_PWM1);
+	LL_TIM_OC_SetMode(Timer,LL_TIM_CHANNEL_CH1,LL_TIM_OCMODE_   ); //Mode ?
+	LL_TIM_OC_SetMode(Timer,LL_TIM_CHANNEL_CH2,LL_TIM_OCMODE_   ); //Mode ?
 	
-	LL_TIM_OC_SetCompareCH1(Timer,ARR/2);
+	// LL_TIM_OC_SetCompareCH1(Timer,ARR/2); Pour definir la période 
 		
 	LL_TIM_EnableAllOutputs(Timer);
 	
--- MDK-ARM/girouette.h
+++ MDK-ARM/girouette.h
@@ -1,1 +1,13 @@
 #include "stm32f103xb.h" 
+#include "stm32f1xx_ll_gpio.h"
+#include "stm32f1xx_ll_bus.h"
+#include "stm32f1xx_ll_usart.h"
+#include "stm32f1xx_ll_tim.h" 
+
+void Girouette_Conf(TIM_TypeDef * Timer);
+
+void Girouette_Start(void);
+
+void Girouette_Stop(void);
+
+void Girouette_Reset(void);
